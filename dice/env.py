3ffbecbbe11d3ab005f4e04eba2305f13b2ff167
zihang37-hue <tangzihang37@gmail.com>
2026-02-10 09:38:20 +0800

    设置维基百科返回结果的单词数量

--- dice/env.py
+++ dice/env.py
@@ -2,8 +2,9 @@ import wikipedia
 
 
 class RobustWikipediaEnv:
-    def __init__(self):
+    def __init__(self, max_chars=3000):
         wikipedia.set_lang("en")
+        self.max_chars = max_chars
 
     def search(self, query):
         print(f"🌍 正在搜索 Wiki: {query}")  # 搜索入口
@@ -15,7 +16,7 @@ class RobustWikipediaEnv:
             page_title = results[0]
             try:
                 page = wikipedia.page(page_title, auto_suggest=False)
-                content = page.summary[:3000].replace("\n", " ")
+                content = page.summary[: self.max_chars].replace("\n", " ")
                 return f"Observation: [Title: {page_title}] {content}..."  # 返回摘要
             except wikipedia.exceptions.DisambiguationError as e:
                 return f"Observation: Ambiguous term '{query}'. Options: {', '.join(e.options[:5])}"  # 歧义页
